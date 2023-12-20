8360092e598c9f755f2c0db3187b50931976e13d
RafaelH <rafael0henkel1@gmail.com>
2023-12-20 20:48:03 -0300

    2- corrigido

--- EXERCICIO-02/script.js
+++ EXERCICIO-02/script.js
@@ -6,7 +6,7 @@
 
 let numerosArray = [1, 25, 67, 101, 133];
 let soma = 0
-for (let i = 0; i <= numerosArray.length; i++) {
+for (let i = 0; i < numerosArray.length; i++) {
   soma += numerosArray[i];
   document.write(soma, "<br>");
 }
